1da676d81a30fd833972f36d5ab6c2e41edbe039
leonardopilapana <leonardopilapana@gmail.com>
2024-06-20 23:26:37 -0500

    SE agrego componente cart y su maquetado html

---
+++ src/app/data/product.data.ts
@@ -0,0 +1,40 @@
+import { Product } from "../models/product";
+
+export const products: Product[] = [
+    {
+        id: 1,
+        name: 'Teclado mecanico Asus RGB',
+        description: 'Algun teclado mecanico con luces RGB',
+        price: 135
+    },
+    {
+        id: 2,
+        name: 'Samsung Smart TV LED 75',
+        description: 'Alguna LCD excelente OLED',
+        price: 1200
+    },
+    {
+        id: 3,
+        name: 'Sony Camara Video',
+        description: 'Alguna camara de video para streaming',
+        price: 230
+    },
+    {
+        id: 4,
+        name: 'Corsair memorias ram 16 GB DDR5',
+        description: 'Memoria RAM para optimizar juegos',
+        price: 400
+    },
+    {
+        id: 5,
+        name: 'Nvidia Asus RTX4900',
+        description: 'Tarjeta de video para juegos',
+        price: 675
+    },
+    {
+        id: 6,
+        name: 'Mouse Dragon500',
+        description: 'Mouse inalambrico gamer',
+        price: 90.5
+    }
+]
